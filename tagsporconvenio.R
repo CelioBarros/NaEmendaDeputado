06e5a692fe68bab98b5a8c7bebdad3423f1e6009
Isabelle Ferreira <isabellecfer@gmail.com>
2016-04-10 18:43:24 -0300

    Corrigindo criação de tags repetidas com c na frente.

--- tagsporconvenio.R
+++ tagsporconvenio.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(stringr)
 library(tm)
+library(jsonlite)
+library(ngram)
 sw = stopwords("pt-br")
 
 dados = read.csv("dadosunidos.csv",sep=";")
@@ -15,12 +17,14 @@ convenios_topwords = data.frame(ID=unique(desc_conv_dados$ID_CONVENIO), TOPWORDS
 
 # Função para retornar ID_CONVENIO  e lista de top tags com valores
 topwords = function(words){
-txt_conv = str_replace_all(words, pattern = "[[:punct:]]", "")
+txt_conv = str_replace_all(words, pattern = "c\\(", "")
+txt_conv = str_replace_all(txt_conv, pattern = "[[:punct:]]", "")
 txt_conv = str_replace_all(txt_conv, pattern = "\\s+", " ")
 txt_list = str_split(txt_conv, pattern = " ")
 title_words = tolower(unlist(txt_list))	
 	
 title_words = title_words[!(tolower(title_words) %in% sw)]
+
 
 top_freqs = data.frame(word = as.factor(title_words)) %>% 
   group_by(word) %>% 
@@ -33,15 +37,24 @@ top_freqs = data.frame(word = as.factor(title_words)) %>%
   res
 }
 
+
 # Juntando descrições de convênios com as mesmas IDs
 desc = with(desc_conv_dados[-3], aggregate(list(TX_OBJETO_CONVENIO = TX_OBJETO_CONVENIO), list(ID_CONVENIO = ID_CONVENIO), paste))
 
 # Iterando em cada conjunto de descrições e gerando as topwords
 for (i in 1:nrow(desc)){
-	convenios_topwords[convenios_topwords$ID==desc[i,]$ID_CONVENIO,]$TOPWORDS = 
-topwords(desc[i,]$TX_OBJETO_CONVENIO)
+	desc2 = topwords(paste(desc[i,]$TX_OBJETO_CONVENIO,sep=" "))
+	convenios_topwords[convenios_topwords$ID==desc[i,]$ID_CONVENIO,]$TOPWORDS = desc2
 }
 
 # Arquivo final com Ids dos convenios e tags
 write.table(convenios_topwords,"id_convenios-tags.csv",sep=";",row.names=F)
+
+
+# Criação do json file
+sink("jsonf.json")
+cat(toJSON(unname(split(convenios_topwords, 1:nrow(convenios_topwords)))))
+sink()
+
+
 	
