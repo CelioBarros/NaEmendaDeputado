a13fdd890c8e615dc0d87af312853ec7c852a344
CelioBarros <celio.barros.filho@gmail.com>
2016-04-10 10:05:56 -0300

    Api retornando os deputados funcionando

--- api/api_deputado.py
+++ api/api_deputado.py
@@ -1,7 +1,41 @@
+# encoding: utf-8
+
 import json
+import csv
+import unicodedata
+import time
+
 
 def info_pessoais_deputado(id_deputado):
-	return json.dumps({"deputado":"Joao Arthur"})
+	
+	info_deputado = {}
+	with open('../data/info_deputados.csv') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=";")
+		for row in reader:
+			autor_id = str(row['Autor.id'])
+			autor_id = force_decode(str(autor_id))
+			autor_id = str(unicodedata.normalize('NFKD', autor_id).encode('ascii','ignore'))
+			if (id_deputado == autor_id):
+				info_deputado = {"nome":force_decode(row['Autor']),"uf":row['Autor..UF.'], "partido":row['Partido']}
+				break
+	return json.dumps(info_deputado)
+
+def force_decode(string, codecs=['utf8', 'cp1252']):
+    for i in codecs:
+        try:
+            return string.decode(i)
+        except:
+            pass
+
+def todos_deputados():
+	lista_deputados = []
+	with open('../data/info_deputados.csv') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=";")
+		for row in reader:
+			info_deputado = {}
+			info_deputado = {"nome":row['Autor'],"uf":row['Autor..UF.'], "partido":row['Partido']}
+			lista_deputados.append(info_deputado)
+	return json.dumps(lista_deputados)
 
 def	convenio(id_convenio):
 	return json.dumps({"convenio":"Saude"})
--- api/main.py
+++ api/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, make_response, request, Response
 import json
 import api_deputado
+import unicodedata
 
 app = Flask(__name__)
 
 
 @app.route('/api/deputado/<id_deputado>')
 def info_pessoais_deputado(id_deputado):
-	response = api_deputado.info_pessoais_deputado(id_deputado)
+	response = api_deputado.info_pessoais_deputado(str(unicodedata.normalize('NFKD', id_deputado).encode('ascii','ignore')))
+	response = make_response(response)
+	response.headers['Access-Control-Allow-Origin'] = "*"
+	return response
+
+@app.route('/api/todos_deputados')
+def todos_deputados():
+	response = api_deputado.todos_deputados()
 	response = make_response(response)
 	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
